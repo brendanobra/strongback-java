7cb74c7ed1c2e5a425cec902aa70b87d2c0c9c95
bobra200 <brendan_obra@comcast.cable.com>
2018-09-28 21:55:55 -0700

    adding PhoenixTalonAdapterTest

---
+++ strongback-tests/src/org/strongback/hardware/PhoenixTalonAdapterTest.java
@@ -0,0 +1,290 @@
+package org.strongback.hardware;
+
+import static org.junit.Assert.*;
+
+public class PhoenixTalonAdapterTest {
+
+    @org.junit.Test
+    public void isFwdLimitSwitchClosed() {
+    }
+
+    @org.junit.Test
+    public void isRevLimitSwitchClosed() {
+    }
+
+    @org.junit.Test
+    public void getDeviceID() {
+    }
+
+    @org.junit.Test
+    public void getOutputCurrent() {
+    }
+
+    @org.junit.Test
+    public void getOutputVoltage() {
+    }
+
+    @org.junit.Test
+    public void getBusVoltage() {
+    }
+
+    @org.junit.Test
+    public void getTemperature() {
+    }
+
+    @org.junit.Test
+    public void getEncPosition() {
+    }
+
+    @org.junit.Test
+    public void getEncVelocity() {
+    }
+
+    @org.junit.Test
+    public void getAnalogInPosition() {
+    }
+
+    @org.junit.Test
+    public void getAnalogInVelocity() {
+    }
+
+    @org.junit.Test
+    public void getPosition() {
+    }
+
+    @org.junit.Test
+    public void getSpeed() {
+    }
+
+    @org.junit.Test
+    public void getFaultForLim() {
+    }
+
+    @org.junit.Test
+    public void getFaultRevLim() {
+    }
+
+    @org.junit.Test
+    public void getFaultForSoftLim() {
+    }
+
+    @org.junit.Test
+    public void getFaultRevSoftLim() {
+    }
+
+    @org.junit.Test
+    public void getFaultHardwareFailure() {
+    }
+
+    @org.junit.Test
+    public void getFaultOverTemp() {
+    }
+
+    @org.junit.Test
+    public void getFaultUnderVoltage() {
+    }
+
+    @org.junit.Test
+    public void getStickyFaultForLim() {
+    }
+
+    @org.junit.Test
+    public void getStickyFaultRevLim() {
+    }
+
+    @org.junit.Test
+    public void getStickyFaultForSoftLim() {
+    }
+
+    @org.junit.Test
+    public void getStickyFaultRevSoftLim() {
+    }
+
+    @org.junit.Test
+    public void getStickyFaultOverTemp() {
+    }
+
+    @org.junit.Test
+    public void getStickyFaultUnderVoltage() {
+    }
+
+    @org.junit.Test
+    public void changeControlMode() {
+    }
+
+    @org.junit.Test
+    public void enableBrakeMode() {
+    }
+
+    @org.junit.Test
+    public void reverseSensor() {
+    }
+
+    @org.junit.Test
+    public void setFeedbackDevice() {
+    }
+
+    @org.junit.Test
+    public void setStatusFrameRateMs() {
+    }
+
+    @org.junit.Test
+    public void setForwardSoftLimit() {
+    }
+
+    @org.junit.Test
+    public void setReverseSoftLimit() {
+    }
+
+    @org.junit.Test
+    public void enableForwardSoftLimit() {
+    }
+
+    @org.junit.Test
+    public void enableReverseSoftLimit() {
+    }
+
+    @org.junit.Test
+    public void enableLimitSwitch() {
+    }
+
+    @org.junit.Test
+    public void enableBrakeMode1() {
+    }
+
+    @org.junit.Test
+    public void configFwdLimitSwitchNormallyOpen() {
+    }
+
+    @org.junit.Test
+    public void configRevLimitSwitchNormallyOpen() {
+    }
+
+    @org.junit.Test
+    public void setVoltageRampRate() {
+    }
+
+    @org.junit.Test
+    public void getFirmwareVersion() {
+    }
+
+    @org.junit.Test
+    public void isSafetyEnabled() {
+    }
+
+    @org.junit.Test
+    public void setSafetyEnabled() {
+    }
+
+    @org.junit.Test
+    public void getExpiration() {
+    }
+
+    @org.junit.Test
+    public void setExpiration() {
+    }
+
+    @org.junit.Test
+    public void isAlive() {
+    }
+
+    @org.junit.Test
+    public void setFeedbackDevice1() {
+    }
+
+    @org.junit.Test
+    public void clearStickyFaults() {
+    }
+
+    @org.junit.Test
+    public void disable() {
+    }
+
+    @org.junit.Test
+    public void enableControl() {
+    }
+
+    @org.junit.Test
+    public void isControlEnabled() {
+    }
+
+    @org.junit.Test
+    public void setSpeed() {
+    }
+
+    @org.junit.Test
+    public void get() {
+    }
+
+    @org.junit.Test
+    public void set() {
+    }
+
+    @org.junit.Test
+    public void getSetpoint() {
+    }
+
+    @org.junit.Test
+    public void clearIaccum() {
+    }
+
+    @org.junit.Test
+    public void reverseOutput() {
+    }
+
+    @org.junit.Test
+    public void setPID() {
+    }
+
+    @org.junit.Test
+    public void setPID1() {
+    }
+
+    @org.junit.Test
+    public void getP() {
+    }
+
+    @org.junit.Test
+    public void getI() {
+    }
+
+    @org.junit.Test
+    public void getD() {
+    }
+
+    @org.junit.Test
+    public void getF() {
+    }
+
+    @org.junit.Test
+    public void getIZone() {
+    }
+
+    @org.junit.Test
+    public void getCloseLoopRampRate() {
+    }
+
+    @org.junit.Test
+    public void setProfile() {
+    }
+
+    @org.junit.Test
+    public void getControlMode() {
+    }
+
+    @org.junit.Test
+    public void changeControlMode1() {
+    }
+
+    @org.junit.Test
+    public void setF() {
+    }
+
+    @org.junit.Test
+    public void setIZone() {
+    }
+
+    @org.junit.Test
+    public void setCloseLoopRampRate() {
+    }
+}
